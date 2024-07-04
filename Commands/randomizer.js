7b4577cfdaab4efbca290c509d77e2f75e67bae8
snovel ice <117592220+SnovelIce@users.noreply.github.com>
2024-07-04 09:01:50 +0300

    Update randomizer.js

--- Commands/randomizer.js
+++ Commands/randomizer.js
@@ -1,40 +1,59 @@
-function NewgenerateNewEnemy() {
-        this.counter = 0;
-        this.enemyPokemon(PokemonFactory.generateWildPokemon(player.route(), player.region, player.subregionObject()));
-        const enemyPokemon = this.enemyPokemon();
-
-
-        enemyPokemon.id = Math.floor(Math.random() * (900 - 1) + 1);
-
-        enemyPokemon.level = Math.floor(Math.random() * (100 - 1) + 1);
-
-        enemyPokemon.name = pokemonList.find(item => item.id === enemyPokemon.id).name
-
-
-        PokemonHelper.incrementPokemonStatistics(enemyPokemon.id, GameConstants.PokemonStatisticsType.Encountered, enemyPokemon.shiny, enemyPokemon.gender, enemyPokemon.shadow);
-        // Shiny
-        if (enemyPokemon.shiny) {
-            App.game.logbook.newLog(LogBookTypes.SHINY, App.game.party.alreadyCaughtPokemon(enemyPokemon.id, true)
-                ? createLogContent.encounterShinyDupe({
-                    location: Routes.getRoute(player.region, player.route()).routeName,
-                    pokemon: enemyPokemon.name,
-                })
-                : createLogContent.encounterShiny({
-                    location: Routes.getRoute(player.region, player.route()).routeName,
-                    pokemon: enemyPokemon.name,
-                }));
-        }
-        else if (!App.game.party.alreadyCaughtPokemon(enemyPokemon.id) && enemyPokemon.health()) {
-            App.game.logbook.newLog(LogBookTypes.NEW, createLogContent.encounterWild({
-                location: Routes.getRoute(player.region, player.route()).routeName,
-                pokemon: enemyPokemon.name,
-            }));
-        }
+PokemonFactory.generateWildPokemon = function(route, region, subRegion) {
+    if (!MapHelper.validRoute(route, region)) {
+        return new BattlePokemon('MissingNo.', 0, PokemonType.None, PokemonType.None, 0, 0, 0, 0, new Amount(0, GameConstants.Currency.money), false, 0, GameConstants.BattlePokemonGender.NoGender, GameConstants.ShadowStatus.None, EncounterType.route);
     }
-    
-
-Battle.generateNewEnemy = NewgenerateNewEnemy;
-
-
-
-
+    let name;
+    const roaming = PokemonFactory.roamingEncounter(route, region, subRegion);
+    if (roaming) {
+        name = pokemonList[Math.floor(Math.random()*pokemonList.length+1)].name
+    }
+    else {
+        name = pokemonList[Math.floor(Math.random()*pokemonList.length+1)].name
+    }
+    const basePokemon = PokemonHelper.getPokemonByName(name);
+    const id = basePokemon.id;
+    const routeAvgHp = (region, route) => {
+        const poke = [...new Set(Object.values(Routes.getRoute(region, route).pokemon).flat().map(p => { var _a; return (_a = p.pokemon) !== null && _a !== void 0 ? _a : p; }).flat())];
+        const total = poke.map(p => pokemonMap[p].base.hitpoints).reduce((s, a) => s + a, 0);
+        return total / poke.length;
+    };
+    // TODO this monster formula needs to be improved. Preferably with graphs :D
+    // Health has a +/- 10% variable based on base health stat compared to the average of the route
+    const maxHealth = Math.round(PokemonFactory.routeHealth(route, region) * (0.9 + (basePokemon.hitpoints / routeAvgHp(region, route)) / 10));
+    const catchRate = this.catchRateHelper(basePokemon.catchRate);
+    const exp = basePokemon.exp;
+    const level = this.routeLevel(route, region);
+    const heldItem = this.generateHeldItem(basePokemon.heldItem, GameConstants.ROUTE_HELD_ITEM_MODIFIER);
+    const money = this.routeMoney(route, region);
+    const shiny = this.generateShiny(GameConstants.SHINY_CHANCE_BATTLE);
+    const gender = this.generateGender(basePokemon.gender.femaleRatio, basePokemon.gender.type);
+    const encounterType = roaming ? EncounterType.roamer : EncounterType.route;
+    if (shiny) {
+        Notifier.notify({
+            message: `✨ You encountered a shiny ${PokemonHelper.displayName(name)()}! ✨`,
+            pokemonImage: PokemonHelper.getImage(id, shiny, basePokemon.gender),
+            type: NotificationConstants.NotificationOption.warning,
+            sound: NotificationConstants.NotificationSound.General.shiny_long,
+            setting: NotificationConstants.NotificationSetting.General.encountered_shiny,
+        });
+    }
+    if (roaming) {
+        Notifier.notify({
+            message: `You encountered a roaming ${name}!`,
+            pokemonImage: PokemonHelper.getImage(id, shiny, basePokemon.gender),
+            type: NotificationConstants.NotificationOption.warning,
+            sound: NotificationConstants.NotificationSound.General.roaming,
+            setting: NotificationConstants.NotificationSetting.General.encountered_roaming,
+        });
+        App.game.logbook.newLog(LogBookTypes.ROAMER, (shiny
+            ? App.game.party.alreadyCaughtPokemon(id, true)
+                ? createLogContent.roamerShinyDupe
+                : createLogContent.roamerShiny
+            : createLogContent.roamer)({
+            location: Routes.getRoute(player.region, player.route).routeName,
+            pokemon: name,
+        }));
+    }
+    const ep = GameConstants.BASE_EP_YIELD * (roaming ? GameConstants.ROAMER_EP_MODIFIER : 1);
+    return new BattlePokemon(name, id, basePokemon.type1, basePokemon.type2, maxHealth, level, catchRate, exp, new Amount(money, GameConstants.Currency.money), shiny, 1, gender, GameConstants.ShadowStatus.None, encounterType, heldItem, ep);
+}
